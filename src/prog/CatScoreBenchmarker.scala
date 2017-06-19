d88230764d2146ad7b03a6c6b6d3af567d755d91
thewildwilli <willivildmand@gmail.com>
2017-06-19 16:50:14 +0100

    Copied MainDock, Benchmarker into SurfaceAtoms, CatScoreBenchmarker.
    SurfaceAtoms is MainDock with added surfaceatom number
    
    CatScoreBenchmarker uses the new numbers to compute an "objective score"
    CatScoreBenchmarker also export in a more excel-friendly way.

---
+++ src/prog/CatScoreBenchmarker.scala
@@ -0,0 +1,154 @@
+package prog
+
+import collection.mutable.Map
+import java.io.{File, FileWriter}
+import java.text.SimpleDateFormat
+import java.util.Date
+
+import profiling.Profiler
+
+// Created by William on 19/06/2017.
+object CatScoreBenchmarker {
+
+  var repeats = 10
+  var commandsPath = "benchmarkcmds.txt"
+  var printClosestRefCount = false
+
+  var initials: Iterable[Int] = List[Int]()
+
+  val csvPath = new SimpleDateFormat("YYYY.MM.dd_HH.mm.ss").format(new Date()) + ".csv"
+
+  def main(args: Array[String]): Unit = {
+    parseBenchmarkerArgs(args)
+    val lines = scala.io.Source.fromFile(commandsPath).getLines.toSeq
+
+    var i = 1
+    for (line <- lines ) if (!line.startsWith("#")) {
+      val (experiment, cmd) = getExperimentAndCmd(line)
+      if (cmd.isEmpty)
+        println()
+      else {
+        if (initials.size > 0) {
+          for (initNumber <- initials) {
+            val extendedCmd = cmd + s" -initials random $initNumber"
+            runExperiment(experiment, i, extendedCmd)
+          }
+        }
+        else runExperiment(experiment, i, cmd)
+      }
+      i += 1
+    }
+    sys.exit(0)
+  }
+
+  private def runExperiment(experiment: String, lineNumber: Int,  cmd: String): Unit = {
+    val dockArgs = Surfaceatoms.parseArgs(cmd.split(" "))
+    print(s"Line $lineNumber: ")
+    Profiler.clear
+    var rmsdAvg = 0.0
+
+    // Count how many times each reference was the closest:
+    val closestRefMap = Map[String, Int]().withDefaultValue(0)
+    var rmsds = List[Double]()
+    var scores = List[Double]()
+    var molAsurfaceatoms = 0
+    var molBsurfaceatoms = 0
+
+    for (_ <- 0 until repeats) {
+      val (_, (closestRef, rmsd), score, molAsurfaceatom, molBsurfaceatom) = Surfaceatoms.doMainDock(dockArgs)
+      print(".")
+      rmsds ::= rmsd
+      scores ::= score
+      molAsurfaceatoms = molAsurfaceatom
+      molBsurfaceatoms = molBsurfaceatom
+      closestRefMap(closestRef) += 1
+    }
+    val avgTime = Profiler.getTimes("dock").toDouble / repeats
+
+    val initNumber = if (dockArgs.initials == "globe") dockArgs.initConfigLevel else dockArgs.initNumber
+    reportToConosole(cmd, avgTime, rmsds, closestRefMap)
+    reportToCsvFile(csvPath, experiment, dockArgs.initials, initNumber, avgTime, rmsds, closestRefMap, molAsurfaceatoms, molBsurfaceatoms, scores)
+  }
+
+  private def getExperimentAndCmd(line: String) = {
+    val trimmed = line.trim()
+    val pos = trimmed.indexOf(':')
+    if (pos < 0)
+      ("", trimmed)
+    else
+      (trimmed.substring(0, pos).trim(), trimmed.substring(pos+1).trim())
+  }
+
+  private def reportToConosole(cmd: String, avgTime: Double, rmsds: Seq[Double],
+                               closestRefMap: Map[String, Int]) = {
+    val rmsdAvg = rmsds.sum / repeats
+    println(s"Average time: ${avgTime}ms, Average RMSD: $rmsdAvg. Line: $cmd")
+    if (printClosestRefCount){
+      println("Times result was closest to each reference:")
+      for ( (refPath, count) <- closestRefMap)
+        println(s"$refPath: $count")
+    }
+  }
+
+  private def reportToCsvFile(csvPath: String, experiment: String,
+                              initType: String, initNumber: Int,
+                              avgTime: Double, rmsds: Seq[Double],
+                              closestRefMap: Map[String, Int],
+                              molAsurfaceatoms: Int,
+                              molBsurfaceatoms: Int,
+                              scores: Seq[Double]): Unit ={
+
+    def writeCSVHeader(csvPath: String): Unit = {
+      println(s"creating $csvPath")
+      val fw: FileWriter = new FileWriter(csvPath, true)
+      try {
+        fw.write(String.join(";", "Experiment", "InitType", "InitNumber", "TimeAvg(ms)", "RMSDAvg",
+          "RMSDStdDev", "RMSDMin", "RMSDMax", "ScoreAvg", "ScoreStdDev", "molBsurfaceatoms", "molBsurfaceatoms", "ObjectiveScore") + "\n")
+      } finally {fw.close()}
+    }
+
+    if (! new File(csvPath).exists()) writeCSVHeader(csvPath)
+
+    val rmsdAvg = rmsds.sum / rmsds.length
+    val rmsdStdDev = stdDev(rmsds)
+    val rmsdMin = rmsds.min
+    val rmsdMax = rmsds.max
+    val scoreAvg = scores.sum / scores.length
+    val scoreStdDev = stdDev(scores)
+    val objective_score = scoreAvg/(molBsurfaceatoms * molAsurfaceatoms)
+
+
+    // write row
+    val fw: FileWriter = new FileWriter(csvPath, true)
+    try {
+      fw.write(String.join(";", experiment, initType, initNumber.toString,
+        avgTime.toString.replace('.', ','), rmsdAvg.toString.replace('.', ','), rmsdStdDev.toString.replace('.', ','),
+        rmsdMin.toString.replace('.', ','), rmsdMax.toString.replace('.', ','), scoreAvg.toString.replace('.', ','), scoreStdDev.toString.replace('.', ','), molAsurfaceatoms.toString,
+        molBsurfaceatoms.toString, objective_score.toString.replace('.', ',')) + "\n")
+    } finally {fw.close()}
+  }
+
+  def parseBenchmarkerArgs(args: Array[String]) = {
+    var i = 0
+
+    while (i < args.length) {
+      args(i) match {
+        case "-r" => repeats = args(i + 1).toInt;i += 2
+        case "-path" => commandsPath = args(i + 1);i += 2
+        case "-closestRefCount" => printClosestRefCount = true; i += 1
+        case "-initials" =>
+          initials = args(i+1).split(",").map(s => s.toInt);
+          i += 2
+        case _ => sys.error("wrong args")
+      }
+    }
+  }
+
+  def variance(xs: Iterable[Double]) = {
+    val mean = xs.sum / xs.size
+    xs.map(x => Math.pow(x-mean, 2)).sum / xs.size
+  }
+
+  def stdDev(xs: Iterable[Double]) = Math.sqrt(variance(xs))
+
+}
---
+++ src/prog/Surfaceatoms.scala
@@ -0,0 +1,342 @@
+package prog
+
+import docking.{Docker, DockLog}
+import docking.dockscore._
+import docking.docksearch._
+import docking.docksearch.forcevector.{DockingParamsHeuristic, ForceVectorDocker, ForceVectorScore}
+import docking.initials.{GlobeInitialsGenerator, RandomInitials}
+import io.Mol2Writer
+import io.threadcso._
+import jmolint.JmolCmds._
+import jmolint.{JmolCmds, JmolFrame, JmolMoleculeReader, JmolPanel}
+import model.{Geometry, Molecule, Rotate}
+import opt.Action
+import profiling.Profiler
+
+
+object Surfaceatoms {
+  val usage = "USAGE: scala DockMain -a (path to A) " +
+    "-b (path to B) " +
+    "-out (B's output path, mol2 format) " +
+    " [-dir d] [--randominit] [-workers w] [-ref a,b,c,...]" +
+    "-docker (ehc|forcevector) [--consolelog] [--nogui] " +
+    " [-initials (globe {angle} {level} ¦ (random {number})]"
+  " [-maxiters m]" +
+    " [--ignoreAhydrogens] " +
+    " [-surface s] " +
+    " [-permeability p]" +
+    " [-balance atomic,electric,hydrogenbond,bondstrength] " +
+    " [-threshold t] " +
+    " [--pdbAddHydrogens] "
+
+  val frame = new JmolFrame(500, 500, false)
+  val jmolPanel = frame.getPanel
+
+  def main(args: Array[String]): Unit = {
+    val dockArgs = parseArgs(args)
+    val (docked, (closestRef, rmsd), score, molAsurfaceatoms, molBsurfaceatoms) = doMainDock(dockArgs)
+
+    new Mol2Writer(dockArgs.fullPathOut).write(docked)      // write docked b to file
+    jmolPanel.openFiles(List(dockArgs.fullPathA, dockArgs.fullPathOut) ++ dockArgs.fullPathsRef)
+    jmolPanel.execSeq(dockArgs.viewInitCmds)
+    println(s"Finished with RMSD: $rmsd ($closestRef), score: $score, molAsurfaceatoms: $molBsurfaceatoms, molBsurfaceatoms: $molBsurfaceatoms")
+
+    Profiler.report
+  }
+
+
+  def doMainDock(dockArgs: DockArgs) = {
+    jmolPanel.openFiles(List(dockArgs.fullPathA, dockArgs.fullPathB), dockArgs.pdbAddHydrogens)
+    jmolPanel.execSync(
+      selectModel("2.1"),
+      setLog(0),
+      zoom(50),
+      save
+    )
+
+    val molA: Molecule = JmolMoleculeReader.read(jmolPanel, 0)
+    val molB: Molecule = JmolMoleculeReader.read(jmolPanel, 1)
+    val docker = getDocker(molA, molB, dockArgs)
+    val dockResult = getDockResult(docker, molA, molB, dockArgs)
+    val docked = dockResult._1
+    val score = dockResult._2
+
+    var num = 0
+    for (atomB <- molB.surfaceAtoms){
+      num = num + 1;
+    }
+
+    val molBsurfaceatoms = num
+
+    num = 0
+    for (atomB <- molA.surfaceAtoms){
+      num = num + 1;
+    }
+
+    val molAsurfaceatoms = num
+    (docked, getRMSD(docked, dockArgs), score, molAsurfaceatoms, molBsurfaceatoms)
+  }
+
+  def getDockResult(docker: Docker, molA: Molecule, molB: Molecule, dockArgs: DockArgs) = {
+    if (dockArgs.liveGui && dockArgs.workers == 1)
+      getDockResultWithLiveUI(docker, molA, molB, dockArgs)
+    else {
+      if (dockArgs.randomInit)
+        doRandomRotation(molB)
+      docker.dock(molA, molB.clone, DockLog.dummy)
+    }
+  }
+
+  def getDockResultWithLiveUI(docker: Docker, molA: Molecule, molB: Molecule, dockArgs: DockArgs) = {
+    val chan = OneOneBuf[Any](5000)
+    val dockLog = new DockLog(chan, enabled = dockArgs.workers == 1)
+
+    if (dockArgs.randomInit)
+      doRandomRotation(molB, chan)
+
+    var dockResult = (null.asInstanceOf[Molecule], 0.0)
+    (proc { dockResult = docker.dock(molA, molB.clone, dockLog); chan.closeOut } ||
+      showActions(chan, jmolPanel, dockArgs))()
+    dockResult
+  }
+
+  def showActions(chan: ?[Any], panel: JmolPanel, dockArgs: DockArgs) = proc {
+    repeat {
+      val msg = chan?()
+      val s = msg match
+      {
+        case a: Action => val cmds = JmolCmds.cmds(a); panel.exec(cmds:_*); cmds
+        case "save" => panel.exec(JmolCmds.save); "save"
+        case "reset" => panel.exec(JmolCmds.reset); panel.execSeq(dockArgs.viewInitCmds); "reset"
+        case other => other.toString
+      }
+      if (dockArgs.consoleLog)
+        println(s)
+      sleepms(100)
+    }
+    chan.closeIn
+  }
+
+  def getRMSD(docked: Molecule, dockArgs: DockArgs) = {
+    jmolPanel.openFiles(dockArgs.fullPathsRef, dockArgs.pdbAddHydrogens)
+    (for (i <- dockArgs.fullPathsRef.indices) yield {
+      val refMol = JmolMoleculeReader.read(jmolPanel, i)
+      (dockArgs.fullPathsRef(i), docked.rmsd(refMol))
+    }).minBy(_._2)
+  }
+
+
+  /**
+    * Gets an instance of a docker given the name. In the future, this could be
+    * enhanced to use reflection.
+    */
+  def getDocker(molA: Molecule, molB: Molecule, dockArgs: DockArgs) = {
+    new MultipleInitialsConcurrentDocker(() => getInnerDocker(molA, molB, dockArgs),
+      getInitialsGenerator(dockArgs), dockArgs.workers)
+  }
+
+  private def getInnerDocker(molA: Molecule, molB: Molecule, dockArgs: DockArgs) = {
+    val ffparams = getFFParams(molA, molB, dockArgs)
+    val scorer =
+      if (dockArgs.scorerName == "ff")
+        new ForceVectorScore(ffparams, 1.25, dockArgs.dockerName == "chain")
+      else
+        new SurfaceDistanceScorer(dockArgs.surface)
+
+    dockArgs.dockerName match {
+      case "atompair" => new AtomPairDocker(new SurfaceDistanceScorer(dockArgs.surface))
+      case "ehc" => new EhcDocker(scorer, dockArgs.maxIters)
+      case "forcevector" => new ForceVectorDocker(ffparams)
+      case "chain" => new FFandEHC(ffparams, scorer, dockArgs.maxIters
+      )
+      case _ => sys.error(usage)
+    }
+  }
+
+  private def getInitialsGenerator(dockArgs: DockArgs) =
+    if (dockArgs.initials == "globe")
+      new GlobeInitialsGenerator(dockArgs.initConfigLevel, dockArgs.initAngle)
+    else if (dockArgs.initials == "random")
+      new RandomInitials(dockArgs.initNumber)
+    else ???
+
+  def getFFParams(molA: Molecule, molB: Molecule, dockArgs: DockArgs) = {
+    val params = DockingParamsHeuristic.estimate(molA, molB)
+    if (dockArgs.balanceIsSet){
+      params.geometricForceWeight = dockArgs.geometricForceWeight
+      params.electricForceWeight = dockArgs.electricForceWeight
+      params.hydrogenBondsForceWeight = dockArgs.hydrogenBondsForceWeight
+      params.bondForceWeight = dockArgs.bondForceWeight
+    }
+    if (dockArgs.surfaceIsSet)
+      params.surface = dockArgs.surface
+    if (dockArgs.permeabilityIsSet)
+      params.permeability = dockArgs.permeability
+    if (dockArgs.ignoreHydrogensIsSet)
+      params.ignoreAHydrogens = dockArgs.ignoreAHydrogens
+    if (dockArgs.thresholdIsSet)
+      params.threshold = dockArgs.threshold
+    params
+  }
+
+  /**
+    * Gets a list of commands from file viewinit.txt.
+    * These commands are then run each time the JMOL view is reset.
+    * If the file does not exist or cannot be read, returns an empty list.
+    */
+  def getViewInitCmds(dockArgs: DockArgs) = {
+    try {
+      scala.io.Source.fromFile(dockArgs.dir + "viewinit.txt").getLines.toSeq
+    } catch {
+      case e: Exception => List[String]()
+    }
+  }
+
+  def parseArgs(args: Array[String]) = {
+    val dockArgs = new DockArgs
+    var i = 0
+
+    try {
+      while (i < args.length) {
+        args(i).trim match {
+          case "" => i += 1
+          case "-a" => dockArgs.pathA = args(i + 1);i += 2
+          case "-b" => dockArgs.pathB = args(i + 1);i += 2
+          case "-out" => dockArgs.pathOut = args(i + 1); i += 2
+          case "-dir" => dockArgs.dir = args(i+1); i += 2
+          case "-workers" => dockArgs.workers = args(i+1).toInt; i += 2; Profiler.setWorkers(dockArgs.workers + 8)
+          case "-ref" => dockArgs.pathRefs = args(i+1); i += 2
+          case "--randominit" => dockArgs.randomInit = true; i += 1
+          case "-docker" => dockArgs.dockerName = args(i + 1); i += 2
+          case "--consolelog" => dockArgs.consoleLog = true; i += 1
+          case "-surface" => dockArgs.surface = args(i + 1).toDouble; dockArgs.surfaceIsSet = true; i += 2
+          case "--pdbAddHydrogens" => dockArgs.pdbAddHydrogens = true; i += 1
+
+          // Initials:
+          case "-initials" =>
+            dockArgs.initials = args(i + 1)
+            dockArgs.initials match {
+              case "globe" =>
+                dockArgs.initAngle = Math.toRadians(args(i + 2).toDouble)
+                dockArgs.initConfigLevel = args(i + 3).toInt
+                i += 4
+              case "random" =>
+                dockArgs.initNumber = args(i+2).toInt
+                i += 3
+              case unknown => sys.error(s"Unknown initials type: $unknown, $usage")
+            }
+
+          // Hill Climbing:
+          case "-maxiters" => dockArgs.maxIters = args(i+1).toInt; i+=2
+          case "-scorer" => dockArgs.scorerName = args(i + 1); i += 2
+
+          // FF:
+          case "-threshold" => dockArgs.threshold = args(i + 1).toDouble; dockArgs.thresholdIsSet = true; i += 2
+          case "-permeability" => dockArgs.permeability = args(i + 1).toDouble; dockArgs.permeabilityIsSet = true; i += 2
+          case "--ignoreAhydrogens" => dockArgs.ignoreAHydrogens = true; dockArgs.ignoreHydrogensIsSet = true; i += 1
+          case "--nogui" => dockArgs.liveGui = false; i += 1
+          case "-balance" =>
+            val balanceStrs = args(i+1).split(',')
+            if (balanceStrs.length != 4)
+              sys.error(usage)
+
+            val atomicForceWeight = balanceStrs(0).toDouble
+            val electricForceWeight = balanceStrs(1).toDouble
+            val hydrogenBondsForceWeight = balanceStrs(2).toDouble
+            val bondForceWeight = balanceStrs(3).toDouble
+            val sum = atomicForceWeight + electricForceWeight + hydrogenBondsForceWeight + bondForceWeight
+
+            if (atomicForceWeight < 0 || electricForceWeight < 0 ||
+              hydrogenBondsForceWeight < 0 || bondForceWeight < 0 || sum==0.0)
+              sys.error(usage)
+
+            dockArgs.geometricForceWeight = atomicForceWeight / sum
+            dockArgs.electricForceWeight = electricForceWeight / sum
+            dockArgs.hydrogenBondsForceWeight = hydrogenBondsForceWeight / sum
+            dockArgs.bondForceWeight = bondForceWeight / sum
+
+            dockArgs.balanceIsSet = true
+            i += 2
+          case unknown => sys.error(s"Unknown argument: $unknown, $usage")
+        }
+      }
+    }
+    catch { case e:Exception => sys.error(s"${e.getMessage}, $usage") }
+
+    if (!dockArgs.valid)
+      sys.error(usage)
+
+    dockArgs.viewInitCmds = getViewInitCmds(dockArgs)
+    dockArgs
+  }
+
+  private def doRandomRotation(m: Molecule, log: ![Any] = null): Unit = {
+    val (axis, angle) = Geometry.randomAxisAndAngle
+    m.rotate(m.getGeometricCentre, axis, angle)
+    if (log != null)
+      log! new Rotate(m.getGeometricCentre, axis, angle)
+  }
+
+  class DockArgs {
+    var dir = ""
+    var pathA = ""
+    var pathB = ""
+    var pathOut = ""
+    var pathRefs = ""
+
+    var dockerName = ""
+    var scorerName = ""
+    var consoleLog = false
+    var liveGui = true
+
+    var initials = "globe"  // either globe or random
+    var initAngle = Math.toRadians(90.0)  // only for globe
+    var initConfigLevel = 0 // only for globe
+    var initNumber = 10  // only for random
+
+
+    var randomInit = false  // shuffle B molecule at the start
+    var workers = 1
+
+    var viewInitCmds = Seq[String]()
+    var pdbAddHydrogens = false
+
+    // Hill Climbing specific
+    var maxIters = Int.MaxValue - 1
+
+    // Force vector docker specifics:
+    var surface = 1.4
+    var surfaceIsSet = false  // becomes true if overridden in program args
+
+    var permeability = 0.5
+    var permeabilityIsSet = false  // becomes true if overridden in program args
+
+    var ignoreAHydrogens = false
+    var ignoreHydrogensIsSet = false  // becomes true if overridden in program args
+
+    var threshold = 1.0e-5
+    var thresholdIsSet = false
+
+    // Force vector force balance: either all 3 set, or all 3 with default values
+    var geometricForceWeight = 0.25
+    var electricForceWeight = 0.25
+    var hydrogenBondsForceWeight = 0.25
+    var bondForceWeight = 0.25
+    var balanceIsSet = false  // becomes true if overridden in program args
+
+    def valid = pathA != "" && pathB != "" && pathOut != "" && dockerName != "" &&
+      Math.abs(geometricForceWeight + electricForceWeight + hydrogenBondsForceWeight + bondForceWeight - 1.0) < 1.0e-5 &&
+      geometricForceWeight >= 0 && electricForceWeight >= 0 && hydrogenBondsForceWeight >= 0 && bondForceWeight >= 0 &&
+      surface >= 0 &&
+      permeability >= 0 && permeability <= 1
+
+    def fullPathA = dir + pathA
+    def fullPathB = dir + pathB
+    def fullPathOut = dir + pathOut
+    def fullPathsRef : Seq[String] =
+      if (pathRefs == "")
+        List[String](fullPathB)
+      else
+        for (p <- pathRefs.split(",")) yield dir + p.trim
+  }
+}
